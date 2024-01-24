503c6770fad32eb30b370fe4d2800d8bfb49ca7c
DavidHuemer <92david.huemer@gmail.com>
2024-01-24 22:18:36 +0100

    Added ExtendedTreeView

---
+++ BaseUI/Extensions/ExtendedTreeView.cs
@@ -0,0 +1,30 @@
+﻿namespace BaseUI.Extensions;
+
+/// <summary>
+/// Extended <see cref="TreeView"/>
+/// </summary>
+public class ExtendedTreeView : TreeView
+{
+    public new static readonly DependencyProperty SelectedItemProperty =
+        DependencyProperty.Register(
+            nameof(SelectedItem),
+            typeof(object),
+            typeof(ExtendedTreeView)
+        );
+
+    public ExtendedTreeView()
+    {
+        SelectedItemChanged += OnSelectedItemChanged;
+    }
+
+    public new object SelectedItem
+    {
+        get => GetValue(SelectedItemProperty);
+        set => SetValue(SelectedItemProperty, value);
+    }
+
+    private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+        SelectedItem = e.NewValue;
+    }
+}
