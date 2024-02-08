bf0b0f9ef79f312e0291d5d4ba77431670bb217e
DavidHuemer <92david.huemer@gmail.com>
2024-02-08 12:21:54 +0100

    Added PlayStatus to Icon converter

---
+++ VideoClipExtractor.Tests/UI/Converters/VideoConverters/PlayStatusToIconConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using Material.Icons;
+using VideoClipExtractor.Data.UI.Video;
+using VideoClipExtractor.UI.Converters.VideoConverters;
+
+namespace VideoClipExtractor.Tests.UI.Converters.VideoConverters;
+
+public class PlayStatusToIconConverterTests
+{
+    private PlayStatusToIconKindConverter _converter = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _converter = new PlayStatusToIconKindConverter();
+    }
+
+    [Test]
+    [TestCase(PlayStatus.Playing, MaterialIconKind.Pause)]
+    [TestCase(PlayStatus.Paused, MaterialIconKind.Play)]
+    public void ConverterReturnsCorrectIcon(PlayStatus playStatus, MaterialIconKind icon)
+    {
+        // Act
+        var result = _converter.Convert(playStatus, typeof(MaterialIconKind), null, CultureInfo.CurrentCulture);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(icon));
+    }
+}
---
+++ VideoClipExtractor.UI/Converters/VideoConverters/PlayStatusToIconKindConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Material.Icons;
+using VideoClipExtractor.Data.UI.Video;
+
+namespace VideoClipExtractor.UI.Converters.VideoConverters;
+
+public class PlayStatusToIconKindConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is PlayStatus playStatus)
+        {
+            return playStatus switch
+            {
+                PlayStatus.Playing => MaterialIconKind.Pause,
+                PlayStatus.Paused => MaterialIconKind.Play,
+                _ => MaterialIconKind.Play
+            };
+        }
+
+        return MaterialIconKind.Play;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
