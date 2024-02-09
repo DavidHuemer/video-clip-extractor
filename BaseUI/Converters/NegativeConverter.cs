a97e17cf911361ff7146418f0c7e11a4911d74df
DavidHuemer <92david.huemer@gmail.com>
2024-02-09 23:34:51 +0100

    Added NegativeConverter and ZoomLevel

---
+++ BaseUI/Converters/NegativeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BaseUI.Converters;
+
+public class NegativeConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not double number) return 0;
+        return number * -1;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BaseUI/Data/ZoomDirection.cs
@@ -0,0 +1,10 @@
+﻿namespace BaseUI.Data;
+
+/// <summary>
+/// The direction of the zoom
+/// </summary>
+public enum ZoomDirection
+{
+    In = -1,
+    Out = 1,
+}
