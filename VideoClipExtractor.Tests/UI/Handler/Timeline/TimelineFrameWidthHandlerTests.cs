1b77152092529d17624b22aa19453e06a9c793e9
DavidHuemer <92david.huemer@gmail.com>
2024-02-09 13:55:17 +0100

    Added TimelineFrameWidthHandler

---
+++ VideoClipExtractor.Tests/UI/Handler/Timeline/TimelineFrameWidthHandlerTests.cs
@@ -0,0 +1,17 @@
+﻿using VideoClipExtractor.UI.Handler.Timeline;
+
+namespace VideoClipExtractor.Tests.UI.Handler.Timeline;
+
+public class TimelineFrameWidthHandlerTests
+{
+    [Test]
+    [TestCase(0, 100)]
+    [TestCase(9, 50)]
+    public void GetFrameWidthReturnsCorrectValue(int zoomLevel, double expected)
+    {
+        // Act
+        var result = TimelineFrameWidthHandler.GetFrameWidth(zoomLevel);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ VideoClipExtractor.UI/Handler/Timeline/TimelineFrameWidthHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace VideoClipExtractor.UI.Handler.Timeline;
+
+public static class TimelineFrameWidthHandler
+{
+    /// <summary>
+    ///     Returns the width of a single frame at a given zoom level
+    /// </summary>
+    /// <param name="zoomLevel"></param>
+    /// <returns>Width of a frame at a given zoom level</returns>
+    public static double GetFrameWidth(int zoomLevel) =>
+        100 / Math.Pow(2, (double)zoomLevel / 9);
+}
