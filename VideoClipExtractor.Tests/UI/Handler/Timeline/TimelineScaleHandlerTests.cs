f689d7db91954abde0917abbae4da4272f5c81d6
DavidHuemer <92david.huemer@gmail.com>
2024-02-09 13:49:55 +0100

    Added TimelineScaleHandler

---
+++ VideoClipExtractor.Tests/UI/Handler/Timeline/TimelineScaleHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using VideoClipExtractor.UI.Handler.Timeline;
+
+namespace VideoClipExtractor.Tests.UI.Handler.Timeline;
+
+public class TimelineScaleHandlerTests
+{
+    [Test]
+    [TestCase(1, 1)]
+    [TestCase(2, 1)]
+    [TestCase(9, 1)]
+    [TestCase(10, 2)]
+    [TestCase(27, 5)]
+    [TestCase(50, 50)]
+    public void GetPrimitiveScaleReturnsCorrectValue(int zoomLevel, int expected)
+    {
+        // Act
+        var result = TimelineScaleHandler.GetPrimitiveScale(zoomLevel);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void NegativeZoomLevelReturnsOne()
+    {
+        // Act
+        var result = TimelineScaleHandler.GetPrimitiveScale(-1);
+
+        Assert.That(result, Is.EqualTo(1));
+    }
+}
---
+++ VideoClipExtractor.UI/Handler/Timeline/TimelineScaleHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace VideoClipExtractor.UI.Handler.Timeline;
+
+public static class TimelineScaleHandler
+{
+    /// <summary>
+    /// Returns the scale factor for the given zoom level
+    /// </summary>
+    /// <param name="zoomLevel">The zoom level for which the scale factor should be returned</param>
+    /// <returns>Scale factor for a given zoom level</returns>
+    public static int GetPrimitiveScale(int zoomLevel)
+    {
+        if (zoomLevel < 1) return 1;
+
+        var factor = zoomLevel / 29;
+        var startIndex = 1 + 30 * factor;
+
+        if (zoomLevel >= startIndex && zoomLevel <= startIndex + 8) return AppendZeroes(1, factor);
+
+        startIndex += 9;
+
+        if (zoomLevel >= startIndex && zoomLevel <= startIndex + 8) return AppendZeroes(2, factor);
+
+        return AppendZeroes(5, factor);
+    }
+
+    private static int AppendZeroes(int number, int count) =>
+        count == 0
+            ? number
+            : number * (int)Math.Pow(10, count);
+}
