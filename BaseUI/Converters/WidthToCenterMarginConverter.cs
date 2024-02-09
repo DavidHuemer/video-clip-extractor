46a1bb947b1de643fea7426cf125b3d4ce72fccc
DavidHuemer <92david.huemer@gmail.com>
2024-02-09 19:45:08 +0100

    Added WidthToCenterMarginConverter

---
+++ BaseUI/Converters/WidthToCenterMarginConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace BaseUI.Converters
+{
+    public class WidthToCenterMarginConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            value is not double width
+                ? new Thickness(0, 0, 0, 0)
+                : new Thickness(-width / 2, 0, 0, 0);
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VideoClipExtractor.Tests/BaseUI/ConverterTests/WidthToCenterMarginConverterTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System.Windows;
+using BaseUI.Converters;
+
+namespace VideoClipExtractor.Tests.BaseUI.ConverterTests;
+
+public class WidthToCenterMarginConverterTests
+{
+    [Test]
+    [TestCase(10.0, -5.0)]
+    [TestCase(0.0, 0.0)]
+    public void ConvertReturnsCorrectValue(double width, double expected)
+    {
+        // Arrange
+        var converter = new WidthToCenterMarginConverter();
+        var expectedThickness = new Thickness(expected, 0, 0, 0);
+
+        // Act
+        var result = converter.Convert(width, typeof(Thickness), null, CultureInfo.CurrentCulture);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedThickness));
+    }
+
+    [Test]
+    public void ConvertWithNoParameterReturnsDefaultMargin()
+    {
+        // Arrange
+        var converter = new WidthToCenterMarginConverter();
+        var expectedThickness = new Thickness(0, 0, 0, 0);
+
+        // Act
+        var result = converter.Convert(null, typeof(Thickness), null, CultureInfo.CurrentCulture);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedThickness));
+    }
+}
