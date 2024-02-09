97b9dad5d7b8872cb0a3b6774a7b434a9306e44f
DavidHuemer <92david.huemer@gmail.com>
2024-02-09 20:28:02 +0100

    Added FrameToMarginConverter

---
+++ VideoClipExtractor.Tests/UI/Converters/VideoConverters/TimelineConverters/FrameToMarginConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+using System.Windows;
+using VideoClipExtractor.UI.Converters.VideoConverters.TimelineConverters;
+
+namespace VideoClipExtractor.Tests.UI.Converters.VideoConverters.TimelineConverters;
+
+public class FrameToMarginConverterTests
+{
+    [Test]
+    [TestCase(0, 1, 0)]
+    public void ConvertFrameToMarginReturnsCorrectValue(int frame, int zoomLevel, double expected)
+    {
+        var converter = new FrameToMarginConverter();
+
+        var result = converter.Convert(new object[] { frame, zoomLevel }, typeof(Thickness), null,
+            CultureInfo.CurrentCulture);
+
+        Assert.That(result, Is.EqualTo(new Thickness(expected, 0, 0, 0)));
+    }
+
+    [Test]
+    public void ConvertFrameWithNoFrameReturnsDefaultMargin()
+    {
+        var converter = new FrameToMarginConverter();
+
+        var result = converter.Convert(new object[] { null, 1 }, typeof(Thickness), null, CultureInfo.CurrentCulture);
+
+        Assert.That(result, Is.EqualTo(new Thickness(0, 0, 0, 0)));
+    }
+
+    [Test]
+    public void ConvertFrameWithNoZoomLevelReturnsDefaultMargin()
+    {
+        var converter = new FrameToMarginConverter();
+
+        var result = converter.Convert(new object[] { 1, null }, typeof(Thickness), null, CultureInfo.CurrentCulture);
+
+        Assert.That(result, Is.EqualTo(new Thickness(0, 0, 0, 0)));
+    }
+}
---
+++ VideoClipExtractor.UI/Converters/VideoConverters/TimelineConverters/FrameToMarginConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using VideoClipExtractor.UI.Handler.Timeline;
+
+namespace VideoClipExtractor.UI.Converters.VideoConverters.TimelineConverters
+{
+    public class FrameToMarginConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (values[0] is not int frame) return new Thickness(0, 0, 0, 0);
+
+            if (values[1] is not int zoomLevel) return new Thickness(0, 0, 0, 0);
+
+            var widthMultiplier = TimelineFrameWidthHandler.GetFrameWidth(zoomLevel);
+
+            var calculatedMargin = frame * widthMultiplier;
+            return new Thickness(calculatedMargin, 0, 0, 0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
