90be44d10377b59d0f9aba04795a03cb0a805328
DavidHuemer <92david.huemer@gmail.com>
2024-02-10 19:18:43 +0100

    Added FrameworkElementWrapper

---
+++ BaseUI/Basics/FrameworkElementWrapper/FrameworkElementWrapper.cs
@@ -0,0 +1,19 @@
+﻿using BaseUI.Events;
+
+namespace BaseUI.Basics.FrameworkElementWrapper;
+
+public class FrameworkElementWrapper : IFrameworkElement
+{
+    private readonly FrameworkElement _element;
+
+    public FrameworkElementWrapper(FrameworkElement element)
+    {
+        _element = element;
+        element.SizeChanged += (sender, args) => SizeChanged?.Invoke(sender, args);
+        element.MouseWheel += (sender, args) => MouseWheel?.Invoke(sender, new MouseWheelEventArgsWrapper(args.Delta));
+    }
+
+    public double ActualWidth => _element.ActualWidth;
+    public event EventHandler? SizeChanged;
+    public event EventHandler<MouseWheelEventArgsWrapper>? MouseWheel;
+}
---
+++ BaseUI/Basics/FrameworkElementWrapper/IFrameworkElement.cs
@@ -0,0 +1,12 @@
+﻿using BaseUI.Events;
+
+namespace BaseUI.Basics.FrameworkElementWrapper;
+
+public interface IFrameworkElement
+{
+    public double ActualWidth { get; }
+
+    public event EventHandler? SizeChanged;
+
+    public event EventHandler<MouseWheelEventArgsWrapper>? MouseWheel;
+}
