f9b696cbe992258d3896b7c9b9bd7bf4a80dc78c
DavidHuemer <92david.huemer@gmail.com>
2024-02-27 11:15:04 +0100

    Added ExtractionTypeToIconConverter

---
+++ VideoClipExtractor.Tests/UI/Converters/ExtractionConverters/ExtractionTypeToIconConverterTest.cs
@@ -0,0 +1,47 @@
+﻿using Material.Icons;
+using VideoClipExtractor.Tests.Basics.Data;
+using VideoClipExtractor.UI.Converters.ExtractionConverters;
+
+namespace VideoClipExtractor.Tests.UI.Converters.ExtractionConverters;
+
+[TestFixture]
+[TestOf(typeof(ExtractionTypeToIconConverter))]
+public class ExtractionTypeToIconConverterTest
+{
+    [SetUp]
+    public void SetUp()
+    {
+        _extractionTypeToIconConverter = new ExtractionTypeToIconConverter();
+    }
+
+    private ExtractionTypeToIconConverter _extractionTypeToIconConverter = null!;
+
+    [Test]
+    public void ImageExtractionReturnsImage()
+    {
+        var imageExtraction = ExtractionExamples.GetImageExtractionExample();
+        var result = _extractionTypeToIconConverter.Convert(imageExtraction, null, null, null);
+        Assert.That(result, Is.EqualTo(MaterialIconKind.Image));
+    }
+
+    [Test]
+    public void VideoExtractionReturnsVideo()
+    {
+        var videoExtraction = ExtractionExamples.GetVideoExtractionExample();
+        var result = _extractionTypeToIconConverter.Convert(videoExtraction, null, null, null);
+        Assert.That(result, Is.EqualTo(MaterialIconKind.Video));
+    }
+
+    [Test]
+    public void NullValueReturnsQuestionMark()
+    {
+        var result = _extractionTypeToIconConverter.Convert(null, null, null, null);
+        Assert.That(result, Is.EqualTo(MaterialIconKind.QuestionMark));
+    }
+
+
+    [Test]
+    public void METHOD()
+    {
+    }
+}
---
+++ VideoClipExtractor.UI/Converters/ExtractionConverters/ExtractionTypeToIconConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Material.Icons;
+using VideoClipExtractor.Data.Extractions;
+
+namespace VideoClipExtractor.UI.Converters.ExtractionConverters
+{
+    public class ExtractionTypeToIconConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+            value switch
+            {
+                ImageExtraction => MaterialIconKind.Image,
+                VideoExtraction => MaterialIconKind.Video,
+                _ => MaterialIconKind.QuestionMark
+            };
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
