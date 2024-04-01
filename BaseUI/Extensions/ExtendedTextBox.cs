5d2940aa08593484d9f545c022ccd9e545f0a6f7
DavidHuemer <92david.huemer@gmail.com>
2024-04-01 18:54:07 +0200

    Added ExtendedTextBox

---
+++ BaseUI/Extensions/ExtendedTextBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Input;
+
+namespace BaseUI.Extensions;
+
+public class ExtendedTextBox : TextBox
+{
+    public ExtendedTextBox()
+    {
+        MouseLeave += OnMouseLeave;
+        MouseEnter += OnMouseEnter;
+    }
+
+    private void OnMouseEnter(object sender, MouseEventArgs e)
+    {
+        var parentWindow = Window.GetWindow(this);
+
+        if (parentWindow != null)
+        {
+            Mouse.RemovePreviewMouseDownHandler(parentWindow, ParentWindow_OnMouseDown);
+        }
+    }
+
+    private void OnMouseLeave(object sender, MouseEventArgs e)
+    {
+        var parentWindow = Window.GetWindow(this);
+
+        if (parentWindow != null)
+        {
+            Mouse.AddPreviewMouseDownHandler(parentWindow, ParentWindow_OnMouseDown);
+        }
+    }
+
+    private void ParentWindow_OnMouseDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+    {
+        var parentWindow = Window.GetWindow(this);
+
+        if (parentWindow != null)
+        {
+            Mouse.RemovePreviewMouseDownHandler(parentWindow, ParentWindow_OnMouseDown);
+        }
+
+        Keyboard.ClearFocus();
+    }
+}
